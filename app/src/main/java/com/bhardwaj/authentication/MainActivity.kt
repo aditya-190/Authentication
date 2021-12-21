f302860ea25de251a558233f85c55a35af7a7058
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-12-21 18:03:59 +0530

    View Binding Added. Fragment Main Created with required buttons.

--- app/src/main/java/com/bhardwaj/authentication/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/authentication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.bhardwaj.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bhardwaj.authentication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- app/src/main/java/com/bhardwaj/authentication/MainFragment.kt
+++ app/src/main/java/com/bhardwaj/authentication/MainFragment.kt
@@ -5,13 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bhardwaj.authentication.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
-        return view
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        clickListeners()
+    }
+
+    private fun clickListeners() {
+        binding.buttonGoogle.setOnClickListener {
+
+        }
+
+        binding.buttonEmail.setOnClickListener {
+
+        }
+
+        binding.buttonPhone.setOnClickListener {
+
+        }
+
+        binding.buttonGithub.setOnClickListener {
+
+        }
+
+        binding.buttonFacebook.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
